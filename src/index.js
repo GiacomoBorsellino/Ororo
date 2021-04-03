3205969afb3c48901189904b6a016ae6469c433b
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-03 11:39:12 +0200

    main

--- src/index.js
+++ src/index.js
@@ -43,8 +43,9 @@ let cityName = document.getElementById("cityName").value; // Valore casella rice
 
 // Chiamata API AICQN
 const response = await fetch("/.netlify/functions/lambda")
-const result = response.json()
-console.log("Hi" + result)
+const result = await response.json()
+const data = await result
+console.log("Hi" + data)
     .then(
         result => {
             console.log(result.data.iaqi.pm10.v)
