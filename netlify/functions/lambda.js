a25f359bab1808c9d30854a199ed69318d35a811
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-03 15:57:30 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,14 +1,17 @@
-exports.handler = async () => {
+exports.handler = async event => {
   const apiKey =  process.env.apiKey
 
-  const fet = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`);
+  const fet = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`)
+  const response = await fet.json() 
 
-  const pass = (response) => {
+  // La funzione fa da back-end: elaboriamo dei dati e li rimandiamo al front-end in formato JSON
+  // con uno statusCode 200, cioè "successo".
+  const pass = (body) => {
     return {
       statusCode: 200,
-      body: JSON.stringify(response)
+      body: JSON.stringify(body)
     }
   }
 
-  return pass(response)
+  return pass(result)
 }
