52380a2a87844d7cb234e7b28445ac29b9eee7b0
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-03 10:11:10 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,15 +1,14 @@
-exports.handler = async event => {
-    const apiKey =  process.env.apiKey;
-  
-    const response = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`)
-    const result = await response.json(); 
-  
-    const pass = (result) => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result)
-      }
+exports.handler = async () => {
+  const apiKey =  process.env.apiKey
+
+  const response = {apiKey: apiKey}
+
+  const pass = (result) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
     }
-  
-    return pass(result)
   }
+
+  return pass(response)
+}
--- src/index.js
+++ src/index.js
@@ -44,6 +44,9 @@ let cityName = document.getElementById("cityName").value; // Valore casella rice
 // Chiamata API AICQN
 await fetch("/.netlify/functions/lambda")
     .then(
+        response => response.json()
+    )
+    .then(
         result => {
             console.log(result.data.iaqi.pm10.v)
             console.log(result.data.iaqi)
