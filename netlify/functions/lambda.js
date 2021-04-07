0d1fffe2675822cd65eb73fbef76f355b978b1f2
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-07 16:23:49 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-const cityName = require("../../src/index");
+const cityName = require("../../build/bundle");
 
 exports.handler = async event => {
   const apiKey =  process.env.apiKey
