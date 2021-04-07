dc804878332d6d340a20cc2b149f042fa77237e8
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-07 15:34:38 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -4,16 +4,16 @@ exports.handler = async event => {
   const apiKey =  process.env.apiKey
 
   const response = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`)
-  const data = await response.json() 
+  const result = await response.json() 
 
-  const pass = (data) => {
+  const pass = (result) => {
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(result)
     }
   }
 
-  return pass(data)
+  return pass(result)
 }
 
 // la restituzione si attiva? .()
