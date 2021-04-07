43b73a6f6bdd71fc35af47bc4b341b17649c7a7f
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-07 11:46:28 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -3,7 +3,9 @@ const fetch = require("node-fetch");
 exports.handler = async event => {
   const apiKey =  process.env.apiKey
 
-  const response = await fetch(`https://api.waqi.info/feed/rome/?token=${apiKey}`)
+  let cityName = document.getElementById("cityName").value; // Valore casella ricerca I
+  
+  const response = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`)
   const data = await response.json() 
 
   const pass = (data) => {
