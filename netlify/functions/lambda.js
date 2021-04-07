203f71066f7f0667f245fca7fb7652e129ca7b67
Giacomo Borsellino <giacomoborsellino@hotmail.it>
2021-04-07 15:29:14 +0200

    main

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,9 +1,10 @@
 const fetch = require("node-fetch");
+let cityName = document.getElementById("cityName").value;
 
 exports.handler = async event => {
   const apiKey =  process.env.apiKey
 
-  const response = await fetch(`https://api.waqi.info/feed/shangai/?token=${apiKey}`)
+  const response = await fetch(`https://api.waqi.info/feed/${cityName}/?token=${apiKey}`)
   const data = await response.json() 
 
   const pass = (data) => {
